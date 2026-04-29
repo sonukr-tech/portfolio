bee11857de1b055d6392e1ee1569103adbd28302
sonukr-tech <sonugupta7203@gmail.com>
2026-04-30 00:30:12 +0530

    update api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://portfolio-backend-t1ns.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
